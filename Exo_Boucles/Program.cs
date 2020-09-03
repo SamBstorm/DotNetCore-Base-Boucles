e10fd821f0a2dd4c850b8589354e5edf15b0c7ad
Samuel Legrain <samuel.legrain@bstorm.be>
2020-09-03 17:19:02 +0200

    Exo_2 - Factorielle

--- Exo_Boucles/Program.cs
+++ Exo_Boucles/Program.cs
@@ -7,17 +7,32 @@ namespace Exo_Boucles
         static void Main(string[] args)
         {
             #region Exo_1 - Suite de Fibonacci
-            int nb0, nb1, nb2;
+            //int nb0, nb1, nb2;
 
-            nb0 = 0;
-            nb1 = 1;
-            for (int i = 0; i < 25; i++)
+            //nb0 = 0;
+            //nb1 = 1;
+            //for (int i = 0; i < 25; i++)
+            //{
+            //    nb2 = nb0 + nb1;
+            //    Console.WriteLine(nb1);
+            //    nb0 = nb1;
+            //    nb1 = nb2;
+            //}
+            #endregion
+
+            #region Exo_2 - Factorielle
+
+            int number;
+            do
             {
-                nb2 = nb0 + nb1;
-                Console.WriteLine(nb1);
-                nb0 = nb1;
-                nb1 = nb2;
+                Console.WriteLine("Veuillez introduire le nombre à soumettre en factorielle : ");
+            } while (!int.TryParse(Console.ReadLine(), out number));
+            int result = 1;
+            for (int i = number; i > 1; i--)
+            {
+                result *= i;
             }
+            Console.WriteLine($"La factorielle de {number} est {result}");
             #endregion
         }
     }
