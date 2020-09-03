ce916662b8df5a38acb8786d57d887f7028a321a
Samuel Legrain <samuel.legrain@bstorm.be>
2020-09-03 16:59:05 +0200

    Exo_1 - Suite de Fibonacci

--- Exo_Boucles/Program.cs
+++ Exo_Boucles/Program.cs
@@ -6,7 +6,19 @@ namespace Exo_Boucles
     {
         static void Main(string[] args)
         {
-            
+            #region Exo_1 - Suite de Fibonacci
+            int nb0, nb1, nb2;
+
+            nb0 = 0;
+            nb1 = 1;
+            for (int i = 0; i < 25; i++)
+            {
+                nb2 = nb0 + nb1;
+                Console.WriteLine(nb1);
+                nb0 = nb1;
+                nb1 = nb2;
+            }
+            #endregion
         }
     }
 }
